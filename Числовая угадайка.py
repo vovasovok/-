27d02239724b77161e1bd0f80e9a7357af404df9
Владимир <argentumfe24work@gmail.com>
2024-01-12 12:44:35 +0300

    Числовая угадайка готова

--- Числовая угадайка.py
+++ Числовая угадайка.py
@@ -5,13 +5,26 @@ def is_valid(str):
     else:
         return False
 randnum = random.randint(1, 101)
-print('Добро пожаловать в числовую угадайку')
-while True:
-    num = input('Введите число от 1 до 100''\n')
-    if not is_valid(num):
-        print('А может быть все таки введем число от 1 до 100?')
-        continue
-    num = int(num)
-    if num<randnum:
-        print('')
+a = 'y'
+while a != 'n':
+    print('Добро пожаловать в числовую угадайку')
+    count = 0
+    while True:
+        num = input('Введите число от 1 до 100''\n')
+        count+=1
+        if not is_valid(num):
+            print('А может быть все таки введем число от 1 до 100?')
+            continue
+        num = int(num)
+        if num<randnum:
+            print('Ваше число меньше загаданного попробуйте еще разок')
+            continue
+        elif num> randnum:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        elif num == randnum:
+            print('Вы угадали, поздравляем!')
+            print('Использовано попыток', count)
+            print('Спасибо что играли в числовую угадайку до свидания')
+            a = input('Хотите ли сыграть еще? Если хотите введите y, если нет то n')
+            break
 
